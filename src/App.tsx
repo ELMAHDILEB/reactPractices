26f76a8c6bf65e703a523aa5dd06f1f5e05e9e2a
ELMAHDILEB <elmahdibelcadi@gmail.com>
2025-09-09 19:06:06 +0000

    pratice fetch data using react with typescript

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useRef, useState } from "react";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+const App = () => {
+  const baseURL = "https://jsonplaceholder.typicode.com/";
+  const [isLoading, setIsLoading] = useState(false);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [page, setPage] = useState(0);
+  const abortControllerRef = useRef<AbortController | null>(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      
+      // cancle any old request that is still working
+      abortControllerRef.current?.abort();
+
+      // create new abort controller
+      abortControllerRef.current = new AbortController();
+
+      try {
+        setIsLoading(true);
+
+        const response = await fetch(`${baseURL}/posts?page=${page}`, {
+          signal: abortControllerRef.current?.signal,  // link abort controller with fetch
+        });
+        
+        if (!response.ok) throw new Error("failed to fetch data");
+        const posts = (await response.json()) as Post[];
+        setPosts(posts);
+
+      } catch (err) {
+        if((err as Error).name === "AbortError"){
+             console.log("Fetch Aborted")
+        }else{
+          setError((err as Error).message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchPosts();
+    return ()=> abortControllerRef.current?.abort();
+
+  }, [page]);
+
+
+  if (error) {
+    return <div style={{ color: "red" }}>Error: {error}</div>;
+  }
+  return (
+    <div>
+      <h1>Data Fetching</h1>
+      <button onClick={()=>setPage(page + 1)}>Increase: ({page})</button>
+       
+      {isLoading && <div>isLoading....</div>}
+        {!isLoading && 
+            <section>
+            {posts.map(({ id, title, body }: Post) => {
+              return (
+                <article key={id}>
+                  <h1>{id}- {title}</h1>
+                  <p>{body}</p>
+                </article>
+              );
+            })}
+          </section>
+        }
+     
+    </div>
+  );
+};
+
+export default App;
